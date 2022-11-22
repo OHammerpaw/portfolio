8ea67599cf2b2305c95b67a10be99541047926c3
lozier <latayroutier@gmail.com>
2022-11-22 14:48:12 -0500

    added template, semantic, ruuning in browser

---
+++ lroutier/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import 'semantic-ui-css/semantic.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Lauren Routier
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
